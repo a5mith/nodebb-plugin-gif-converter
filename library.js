2ee22d8d32324df129986714bc166d80bcbb3a0e
a5mith <aaronsmith91@sky.com>
2014-08-18 03:56:29 +0100

    additional logging

--- library.js
+++ library.js
@@ -78,7 +78,7 @@
                     callback(null, raw += html);
                 });
             } else {
-                winston.warn('Couldn\'t Filter');
+                winston.warn('Couldn\'t Filter gifconvertinfo');
                 callback(null, raw);
             }
         });
