ab9656d0ac2cb98b5ee2c40b22d90612219fe3ee
a5mith <aaronsmith91@sky.com>
2014-08-18 05:26:10 +0100

    regex long

--- library.js
+++ library.js
@@ -6,7 +6,7 @@
         winston = module.parent.require('winston'),
         S = module.parent.require('string'),
         meta = module.parent.require('./meta'),
-        gifconvertRegex = (/\.(gif)$/i),
+        gifconvertRegex = /(?:(?:(?:[A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)(?:(?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)(?:jpg|gif|png)/,
         Embed = {},
         cache, appModule;
     var getgifconvert = function(gifconvertKey, callback) {
